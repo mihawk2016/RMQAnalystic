14ba47541957f6e8ef79a56b4b06ba271a0c3e64
Zhang <z.zhang@miteke.com>
2017-02-07 18:23:56 +0800

    usd pip

--- TEST/TEST_newVersion2.R
+++ TEST/TEST_newVersion2.R
@@ -1,21 +1,48 @@
 
 rm(list = ls())
 
-# detach('package:RMitekeLab', unload = TRUE)
-# detach('package:parallel', unload = TRUE)
+package.list <- search()
+# if ('package:RMitekeLab' %in% package.list) {
+#   detach('package:RMitekeLab', unload = TRUE)
+# }
+# if ('package:parallel' %in% package.list) {
+#   detach('package:parallel', unload = TRUE)
+# }
 # library(RMitekeLab)
 # library(parallel)
-
+library(XML)
+library(xml2)
 source('./NewVersion/functions2.R')
 
 files <- file.path('.', 'TEST_FILE', dir('TEST_FILE'))[2]
 
 # cl <- makeCluster(detectCores())
+# # clusterExport(cl, files)
 # 
 # time <- system.time({
-#   infos <- p.read.mq.file(cl, files)
+#   infos <- read.mq.file(files, cl)
 # })
-# print(time) 
+# print(time)
 # ## 8.56 Secs
 
-print(TT <- read.mq.file(files))
+
+# time.new <- system.time({
+#   # table.new <- read.mq.file(files)
+#   table.new <- readHTMLTable(files, colClasses = c(rep('character', 23)), stringsAsFactors = FALSE, encoding = 'UTF-8', which = 1)
+# })
+# print(time.new)
+# ## 2.85 Secs
+
+
+
+time.old <- system.time({
+  # parse <- htmlParse(files)
+  # parse <- read_html(files, encoding = 'GBK')
+  # table.old <- lapply(files, readHTMLTable)
+  # table.old <- readHTMLTable(files, stringsAsFactors = FALSE, encoding = 'UTF-8', which = 1)
+  old.data <- read.mq.file(files)
+})
+print(time.old)
+
+## 31.58 Secs for parLapply(cl, xx, read.mq.file)
+## 87.06 Secs for lapply(xxx, read.mq.file)
